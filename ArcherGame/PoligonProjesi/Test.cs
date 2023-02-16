529a318712e8e44bf7defbf93e2653ba66ff9c11
Sebsson <Sebastian@divisor.se>
2023-02-16 12:00:10 +0100

    Commiting Test file

---
+++ ArcherGame/PoligonProjesi/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoligonProjesi
+{
+    internal class Test
+    {
+        static public void TestText()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
